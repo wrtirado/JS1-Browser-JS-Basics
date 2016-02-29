dcabf39264b5a866e21a3743f930a3b39b173f64
Student <Student@Mac-mini-7.local>
2016-02-29 12:00:17 -0700

    Part two of JS1, Week 2

--- JS1.js
+++ JS1.js
@@ -1,1 +1,8 @@
-alert ("Hello Boulder!")
+// alert ("Hello Boulder!")
+// Use var, prompt, string literals, the + operator, and alert such
+// that when the user opens the page, it will ask them for their
+// name, and then respond with "Hello [YOUR_NAME]!"
+
+var userName = prompt("Please, enter you name!")
+
+alert ("Hello " + userName + "!")
